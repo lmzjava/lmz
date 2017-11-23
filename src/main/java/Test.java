57d8f0c8fb2cf995885a29ee03d3001a95873a69
“mingzhu <mingzhu.li@hand-china.com>
2017-11-23 16:51:01 +0800

    ff

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,7 +1,8 @@
-package PACKAGE_NAME;
-
 /**
  * Created by Administrator on 2017/11/23 0023.
  */
 public class Test {
+    public static void main(){
+        System.out.println("hello world");
+    }
 }
